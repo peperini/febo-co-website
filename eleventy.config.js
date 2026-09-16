5e68b685b8f6247f38e9c9454e517622d57811b5
Jose Felix <jarmando.felix@icloud.com>
2026-09-16 14:59:30 -0700

    add github PR templates

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+// Essential module imports for the configuration
+import path from 'path'
+import htmlmin from 'html-minifier'
+import EleventyVitePlugin from '@11ty/eleventy-plugin-vite'
+import pugPlugin from '@11ty/eleventy-plugin-pug'
+import { VitePWA } from 'vite-plugin-pwa'
+import glslPlugin from 'vite-plugin-glsl'
+
+export default async function (eleventyConfig) {
+
+}
